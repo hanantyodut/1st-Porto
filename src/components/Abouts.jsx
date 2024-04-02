0752eb13d060958dffefdd55a01b5e426dd725cc
hanantyodut <hanantyodianutomo@gmail.com>
2024-04-02 15:32:30 +0700

    add edu & work

--- src/components/Abouts.jsx
+++ src/components/Abouts.jsx
@@ -31,9 +31,24 @@ const Abouts = () => {
 							Education
 						</p>
 						<div className="w-full">
-							<ul className="flex flex-col ">
-								<li className="flex justify-between ">
+							<ul className="flex flex-col mt-6">
+								<li className="flex justify-between">
+									<div className="ml-[36px]">
+										<img
+											src="./src/assets/colored-ugm.png"
+											className="w-[90px]"
+										/>
+									</div>
 									<div>
+										<p className="mt-4 text-lg font-semibold">
+											Gadjah Mada University
+										</p>
+										<p className=" font-semibold">Mechanical Engineering</p>
+										<p className="italic font-semibold">2016-2020</p>
+									</div>
+								</li>
+								<li className="flex justify-between mt-4">
+									<div className="ml-8">
 										<img src="./src/assets/logo-81.jpg" className="w-[100px]" />
 									</div>
 									<div>
@@ -44,17 +59,6 @@ const Abouts = () => {
 										<p className="italic font-semibold">2014-2016</p>
 									</div>
 								</li>
-								<li className="flex justify-between ">
-									<div>
-										<img src="" />
-									</div>
-									<div>
-										<p className="mt-4 text-lg font-semibold">
-											SMAN 81 Jakarta Timur
-										</p>
-										<p className="italic font-semibold">2014-2016</p>
-									</div>
-								</li>
 							</ul>
 						</div>
 					</div>
@@ -62,6 +66,42 @@ const Abouts = () => {
 						<p className="text-2xl font-bold inline-block border-b-4 border-[#ffd700]">
 							Previous Work Experience
 						</p>
+						<div className="w-full">
+							<ul className="flex flex-col mt-6">
+								<li className="flex justify-between ">
+									<div>
+										<p className="mt-4 text-lg font-semibold">
+											Elsewedy Electric Indonesia
+										</p>
+										<p className=" font-semibold">
+											Jr. Quality Engineer - Metal Part
+										</p>
+										<p className="italic font-semibold">2022-2024</p>
+									</div>
+									<div className="ml-8">
+										<img
+											src="./src/assets/logo-elsewedy.jpg"
+											className="w-[130px] mt-4"
+										/>
+									</div>
+								</li>
+								<li className="flex justify-between mt-4">
+									<div>
+										<p className="mt-4 text-lg font-semibold">
+											Ohgishi Indonesia
+										</p>
+										<p className=" font-semibold">PPIC Staff</p>
+										<p className="italic font-semibold">2021-2022</p>
+									</div>
+									<div className="ml-[36px]">
+										<img
+											src="./src/assets/ohgishi-2.png"
+											className="w-[100px] pt-4 mr-4"
+										/>
+									</div>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
